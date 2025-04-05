943a08dababcab5704044013c11043b3fe78771c
pratikbagdi <96934729+pratikbagdi@users.noreply.github.com>
2025-04-05 23:58:43 +0530

    Add files via upload

---
+++ app/layout.js
@@ -0,0 +1,49 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
+import Header from "@/components/header";
+import { ThemeProvider } from "@/components/theme-provider";
+import { dark } from "@clerk/themes";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Sensai",
+  description: "AI Career Coach",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <head>
+          <link rel="icon" href="/logo.png" sizes="any" />
+        </head>
+        <body className={`${inter.className}`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            <main className="min-h-screen">{children}</main>
+            <Toaster richColors />
+
+            <footer className="bg-muted/50 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p>&copy; 2025 SensAI. All rights reserved.</p>
+                <p>Powered by AI for Smarter Career Guidance</p>
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
